1d069fe071397ba3c1447fef03b98831ccbf3b6f
Gabriella Costa <gabx2205@gmail.com>
2024-08-19 23:15:25 -0300

    feat: error page

--- project3--harry-potter/src/pages/Error/index.jsx
+++ project3--harry-potter/src/pages/Error/index.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import Footer from '../../components/Footer'
+import style from './style.module.css'
+import logo from '../../assets/logo.png'
+import Button from '../../components/Button'
+import { useNavigate } from 'react-router-dom'
 
 function Error() {
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate('/')
+  }
+
   return (
-    <div>Error</div>
+    <div className={style.body}>
+      <div className={style.mainContent}>
+        <header>
+          <img src={logo} alt="harry potter logo" className={style.logo}/>
+        </header>
+        <main>
+          <h1>Error 404 - Page not found</h1>
+          <Button value={'Back To Home'} onClick={handleClick}/>
+        </main>
+      </div>
+      <Footer />
+    </div>
   )
 }
 
