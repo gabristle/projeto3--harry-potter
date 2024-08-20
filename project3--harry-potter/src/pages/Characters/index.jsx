708c8778dd829ba4ecea205c227ed4a496b976f1
Gabriella Costa <gabx2205@gmail.com>
2024-08-19 22:40:32 -0300

    feat: characters page

--- project3--harry-potter/src/pages/Characters/index.jsx
+++ project3--harry-potter/src/pages/Characters/index.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import style from './style.module.css'
 import Card from '../../components/Card'
+import Footer from '../../components/Footer'
+import logo from '../../assets/logo.png'
+import Button from '../../components/Button'
+import Input from '../../components/Input'
 
 function Characters() {
   return (
-    <>
-    </>
+    <div className={style.body}>
+      <div className={style.mainContent}>
+        <header>
+          <img src={logo} alt="harry potter logo" className={style.logo}/>
+        </header>
+        <main>
+          <div className={style.actionsContainer}>
+            <Button value={'Add New Character'}/>
+            <Input type={'text'} placeholder={'Search...'} id={'search'}/>
+          </div>
+          <div className={style.cardsContainer}>
+            
+          </div>
+        </main>
+      </div>
+      <Footer />
+    </div>
   )
 }
 
