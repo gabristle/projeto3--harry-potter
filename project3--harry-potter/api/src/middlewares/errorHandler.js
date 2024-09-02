68595b844b953615541fa495f0749bad1a40a6e3
Gabriella Costa <gabx2205@gmail.com>
2024-09-02 18:43:22 -0300

    fix: adjustments

--- project3--harry-potter/api/src/middlewares/errorHandler.js
+++ project3--harry-potter/api/src/middlewares/errorHandler.js
@@ -1,9 +1,9 @@
-import logger from '../../logger.js'
+import logger from '../../logger.js';
 
 const errorHandler = (err, req, res, next) => {
     logger.error(`Error occurred: ${err.message}`);
     res.on('finish', () => {
-        logger.error(`Response: ${err.message} ${err.method} ${err.originalUrl}`);
+        logger.error(`Response: ${err.message} ${req.method} ${req.originalUrl}`);
     });
     res.status(500).send('Internal Server Error');
 };
