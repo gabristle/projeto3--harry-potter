afc52f23791e801375811a65aae27899b786ac5a
Gabriella Costa <gabx2205@gmail.com>
2024-09-02 16:52:40 -0300

    fix: actions adjustments

--- project3--harry-potter/api/src/routes/character.js
+++ project3--harry-potter/api/src/routes/character.js
@@ -1,13 +1,14 @@
 import express from 'express'
 import { characterController } from '../controllers/Character.js'
 import { characterMiddleware } from '../middlewares/characterMiddleware.js'
+import { authenticateToken } from '../middlewares/authMiddleware.js'
 
 const router = express.Router()
 
-router.get('/search', characterController.search)
-
 router.get('/', characterController.listAll)
 
-router.post('/', characterMiddleware, characterController.add)
+router.get('/:name', authenticateToken, characterController.search)
+
+router.post('/', authenticateToken, characterMiddleware, characterController.add)
 
 export default router
